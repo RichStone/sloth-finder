28a7aaba5f81eb1149697d10ef58db28d9c49c33
Rich Steinmetz <hey@richsteinmetz.com>
2024-06-03 18:39:09 +0200

    Log results

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -1,1 +1,30 @@
-# goes through all pages and finds all with the specified keywords
+class Finder
+  attr_writer :keywords, :first, :last
+
+  def keyword_mentions
+    scope = ScrapedPage.all
+    scope = scope.first(@first) if @first
+    scope = scope.last(@last) if @last
+
+    scope.each do |page|
+      result = page.keyword_mentions(keywords: @keywords)
+      if result
+        puts "✅ HIT #{page.url}"
+        puts result[:related_texts]
+        puts "\n"
+      else
+        puts "❌ MISS #{page.url}"
+      end
+
+      puts "-" * 60
+    end
+  end
+
+  private
+
+  def initialize(keywords:, first: nil, last: nil)
+    @keywords = keywords
+    @first = first
+    @last = last
+  end
+end
--- app/models/scraped_page.rb
+++ app/models/scraped_page.rb
@@ -1,3 +1,20 @@
 class ScrapedPage < ApplicationRecord
   validates :url, presence: true, uniqueness: true
+
+  def keyword_mentions(keywords: [])
+    return nil if keywords.empty?
+
+    regex = /\b(#{keywords.join('|')})\b/i
+    doc = Nokogiri::HTML(body)
+    texts = doc.xpath('//p | //a | //div').map(&:text).map(&:strip).uniq
+
+    matches = texts.select { |text| text.match?(regex) }
+
+    if matches.any?
+      related_texts = matches.join("\n\n")
+      { related_texts: related_texts }
+    else
+      nil
+    end
+  end
 end
