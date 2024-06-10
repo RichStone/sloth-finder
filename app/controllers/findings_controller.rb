0ffb7f121e44626c4d8ad15d1909fb2a7a54d777
Rich Steinmetz <hey@richsteinmetz.com>
2024-06-10 19:48:03 +0200

    Add alert and strong params

--- app/controllers/findings_controller.rb
+++ app/controllers/findings_controller.rb
@@ -3,17 +3,29 @@ class FindingsController < ApplicationController
   end
 
   def create
-    keywords = params[:keywords].split(",").map(&:strip)
-    finder = Finder.new(keywords: keywords)
-    @results = finder.find_matches
-    # Reverse the order of the results so that the most recent Ruby Weekly issues
-    # come first because it's currently scraped from the oldest to the most recent.
-    # Not ideal, but we can worry about it later.
-    @results.reverse
+    keywords = sanitize_keywords(findings_params[:keywords])
 
-    respond_to do |format|
-      format.turbo_stream
-      format.html {}
+    if keywords.empty?
+      flash.now.alert = "Please enter at least one keyword."
+      render :index, status: :unprocessable_entity
+    else
+      finder = Finder.new(keywords: keywords)
+      @results = finder.find_matches
+      @results.reverse
+
+      respond_to do |format|
+        format.turbo_stream
+      end
     end
+  end
+
+  private
+
+  def findings_params
+    params.require(:findings).permit(:keywords)
+  end
+
+  def sanitize_keywords(keywords)
+    keywords.split(",").map { |keyword| ActionController::Base.helpers.sanitize(keyword.strip) }.reject(&:blank?)
   end
 end
