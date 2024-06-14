13f51628aee8edd0a354dab097075cca255847df
Rich Steinmetz <hey@richsteinmetz.com>
2024-06-14 17:08:39 +0200

    Add more facts

--- app/helpers/sloth_facts_helper.rb
+++ app/helpers/sloth_facts_helper.rb
@@ -4,11 +4,15 @@ module SlothFactsHelper
       "🦥 My results are stellar. What are yours?",
       "🦥 No worries, sloths are the only thing that matters.",
       "🦥 I'm urgently chilled. You seem not.",
+      "🦥 You seem heated. I'm cool. Like 30-34 degrees Celsius cool.",
       "🦥 Sloth out bro; and try some good keywords.",
       "🦥 I'm not sure what you're looking for. But I'm sure you're not looking for me.",
       "🦥 Slothing is the new black.",
       "🦥 Not today, not today, not today...",
-      "🦥 Maybe tomorrow..."
+      "🦥 Maybe tomorrow...",
+      "🦥 Sloths have a mutualistic ectosymbiotic relationship with the flora and fauna growing on their backs, consisting of fungi, algae, and moths.",
+      "🦥 Sloths are two- or three-toed. To look better, they have four toes in Zootopia DALL-E style.",
+      "🦥 I go 240 metres per hour. You just sit."
     ]
   end
 end
